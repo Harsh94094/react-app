1b7326f810a97c05bdce7ebab47ef49be852e5ec
Harsh94094 <harshgadhiya5949@gmail.com>
2023-04-22 12:19:49 +0530

    first commit

---
+++ app1/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+var output = (
+  <div>
+    <h1>Resume is a here..</h1>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-12'>
+          
+        </div>
+      </div>
+    </div>
+  </div>
+
+)
+
+
+root.render(output);  
+
